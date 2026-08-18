4788289b98f5f61ea6f2a80bc30bff8f486043e4
MiNiGeospatial <mike.nibelle@gmail.com>
2026-08-18 09:28:59 +0100

    💄 Use warm burnout colours

--- nvim/lua/plugins/colors.lua
+++ nvim/lua/plugins/colors.lua
@@ -1,33 +1,41 @@
+-- return {
+--   "folke/tokyonight.nvim",
+--   priority = 1000,
+--   version = "*",
+--   opts = {
+--     transparent_background = true, -- disables setting the background color.
+--     dim_inactive = {
+--       enabled = true,              -- dims the background color of inactive window
+--       shade = "dark",
+--       percentage = 0.08,           -- percentage of the shade to apply to the inactive window
+--     },
+--     show_end_of_buffer = true,
+--     highlight_overrides = {
+--       all = function(colors)
+--         return {
+--           CursorLineNr = {
+--             fg = colors.peach,
+--           }
+--         }
+--       end
+--     },
+--     intergrations = {
+--       gitsigns = true,
+--       telescope = true,
+--       neogit = true,
+--       nvim_surround = true,
+--       telescope = {
+--         enabled = true,
+--       },
+--       mason = true
+--     },
+--   }
+-- }
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  version = "*",
-  opts = {
-    transparent_background = true, -- disables setting the background color.
-    dim_inactive = {
-      enabled = true,              -- dims the background color of inactive window
-      shade = "dark",
-      percentage = 0.08,           -- percentage of the shade to apply to the inactive window
-    },
-    show_end_of_buffer = true,
-    highlight_overrides = {
-      all = function(colors)
-        return {
-          CursorLineNr = {
-            fg = colors.peach,
-          }
-        }
-      end
-    },
-    intergrations = {
-      gitsigns = true,
-      telescope = true,
-      neogit = true,
-      nvim_surround = true,
-      telescope = {
-        enabled = true,
-      },
-      mason = true
-    },
-  }
+	"felipefdl/warm-burnout",
+	priority = 1000,
+	config = function(plugin)
+		vim.opt.rtp:append(plugin.dir .. "/nvim")
+		vim.cmd.colorscheme("warm-burnout-dark")
+	end,
 }
